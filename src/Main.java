024828cc9ef13f38aad3d0c4df4ae6661196642e
fforay <fforay@users.noreply.github.com>
2023-09-28 11:55:06 +0200

    Hi

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+        System.out.println("Merci :)");
+
         // Press Maj+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
